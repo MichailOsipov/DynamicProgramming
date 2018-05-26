13373735030c1559b594ce03dc7fe4d0f1028f43
Michail <osimike96@gmail.com>
2018-05-26 18:38:34 +0300

    Errors fix.

--- dynamicProgramming.py
+++ dynamicProgramming.py
@@ -1,10 +1,10 @@
 import numpy as np
-import math
+import matplotlib.pyplot as plt
 
 moneyNeedToWin = 100
-probabilityToGetEagle = 0.55
-gamma = 0.5
-precision = 0.00001
+probabilityToGetEagle = 0.25
+gamma = 1
+precision = 1e-3
 
 def throwCoin(moneyToBet):
     randomNumber = np.random.uniform(0, 1)
@@ -24,39 +24,78 @@ def probabilityToGetMoney(currentMoney, nextMoney, moneyToBet):
 def valuesYouWillGet(currentMoney, nextMoney, moneyToBet):
     if currentMoney < moneyToBet:
         return 0
-    if currentMoney + nextMoney > moneyNeedToWin:
+    if currentMoney + moneyToBet != nextMoney:
+        return 0
+    if currentMoney + moneyToBet >= moneyNeedToWin:
         return 1
     return 0
 
 def needToStopFunc(prevValues, newValues):
-    for i in range(0, len(prevValues)):
-        if abs(prevValues[i] - newValues[i]) > precision:
-            return False
-    return True
-    
-def startSimulation():
-    # 0 means 1
-    stateValues = np.zeros(moneyNeedToWin - 1)
+    maxDiff = np.amax(np.absolute(prevValues - newValues))
+    if (maxDiff < precision):
+        return True
+    return False
+
+def calculateValues():
+    stateValues = np.zeros(moneyNeedToWin + 1)
     needToStop = False
     while needToStop != True:
-        # 0 means 1
-        currStateValues = np.zeros(moneyNeedToWin - 1)
+        currStateValues = np.zeros(moneyNeedToWin + 1)
         for s in range(1, moneyNeedToWin):
-            stateValue = 0
+            stateValue = -1
             
-            for a in range(0, min(s, moneyNeedToWin - s)):
-                aVal = a + 1
-                strategyRes = throwCoin(aVal)                
-                secondSum = 0
-                for sNext in range (1, moneyNeedToWin):
-                    p = probabilityToGetMoney(s, sNext, aVal)
-                    r = valuesYouWillGet(s, sNext, a)
-                    secondSum = secondSum + p * (r + gamma * stateValues[s - 1])
+            for a in range(1, min(s, moneyNeedToWin - s) + 1):
+                sNext1 = s + a
+                sNext2 = s - a
+
+                p1 = probabilityToGetMoney(s, sNext1, a)
+                r1 = valuesYouWillGet(s, sNext1, a)
+                nextStateValue1 = stateValues[sNext1]
+                curr1 = p1 * (r1 + gamma * nextStateValue1)
                 
-                stateValue = stateValue + strategyRes * secondSum
-            currStateValues[s - 1] = stateValue
+                p2 = probabilityToGetMoney(s, sNext2, a)
+                r2 = valuesYouWillGet(s, sNext2, a)
+                nextStateValue2 = stateValues[sNext2]
+                curr2 = p2 * (r2 + gamma * nextStateValue2)
+
+                curr = curr1 + curr2
+                if curr > stateValue:
+                    stateValue = curr
+
+            currStateValues[s] = stateValue
         needToStop = needToStopFunc(stateValues, currStateValues)
         stateValues = currStateValues
-        print(stateValues)
+    return stateValues
 
-startSimulation()
+def calculateStrategy(strategyValues):
+    strategy = np.zeros(moneyNeedToWin + 1)
+    for s in range (1, moneyNeedToWin):
+        stateValue = -1
+        currA = 0
+        for a in range(1, min(s, moneyNeedToWin - s) + 1):
+            sNext1 = s + a
+            sNext2 = s - a
+            
+            p1 = probabilityToGetMoney(s, sNext1, a)
+            r1 = valuesYouWillGet(s, sNext1, a)
+            nextStateValue1 = strategyValues[sNext1]
+            curr1 = p1 * (r1 + gamma * nextStateValue1)
+            
+            p2 = probabilityToGetMoney(s, sNext2, a)
+            r2 = valuesYouWillGet(s, sNext2, a)
+            nextStateValue2 = strategyValues[sNext2]
+            curr2 = p2 * (r2 + gamma * nextStateValue2)
+
+            curr = curr1 + curr2
+            if curr > stateValue:
+                stateValue = curr
+                currA = a
+        strategy[s] = currA
+    return strategy
+        
+strategyValues = calculateValues()
+strategy = calculateStrategy(strategyValues)
+
+#plt.plot(np.arange(len(strategyValues)), strategyValues)
+plt.plot(np.arange(len(strategy)), strategy)
+plt.show()
